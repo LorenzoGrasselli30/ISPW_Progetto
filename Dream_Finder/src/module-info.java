49e19dca74fd9d842165d7d610e87ca01c6ec33e
LorenzoGrasselli30 <lorenzo.grasselli30@gmail.com>
2025-11-19 14:35:43 +0100

    DreamFinder

---
+++ Dream_Finder/src/module-info.java
@@ -0,0 +1,9 @@
+module Dream_Finder {
+	exports application.controller.graphic;
+	exports application;
+	exports application.configuration;
+
+	requires javafx.base;
+	requires javafx.fxml;
+	requires javafx.graphics;
+}
