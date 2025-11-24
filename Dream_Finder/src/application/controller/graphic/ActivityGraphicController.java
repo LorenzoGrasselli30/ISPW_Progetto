9cda7d2aa65e4a2233c6175cd5eda7d412868dcb
LorenzoGrasselli30 <lorenzo.grasselli30@gmail.com>
2025-11-24 16:51:27 +0100

    Rm Flowpane

--- Dream_Finder/src/application/controller/graphic/ActivityGraphicController.java
+++ Dream_Finder/src/application/controller/graphic/ActivityGraphicController.java
@@ -1,20 +1,26 @@
 package application.controller.graphic;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import application.view.WindowsNavigatorUtils;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class ActivityGraphicController {
-	
-	
+    
 	@FXML
 	public void goToHomepage(ActionEvent event) throws IOException {
 		String fxmlFile = "homeView.fxml";
