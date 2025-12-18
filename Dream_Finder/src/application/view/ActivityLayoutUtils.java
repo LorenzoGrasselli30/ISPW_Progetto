5bbf4f25cf01de7b1311015c9a95300d109fbd5c
LorenzoGrasselli30 <lorenzo.grasselli30@gmail.com>
2025-12-18 23:21:15 +0100

    ActivityLayoutUtils

---
+++ Dream_Finder/src/application/view/ActivityLayoutUtils.java
@@ -0,0 +1,22 @@
+package application.view;
+
+import java.io.IOException;
+
+import application.model.bean.ActivityDTO;
+import javafx.geometry.Pos;
+import javafx.scene.Cursor;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+public class ActivityLayoutUtils {
+	//Impedisco la creazione di istanze
+	private ActivityLayoutUtils() {
+		throw new IllegalStateException("AlertUtils class");
+	}
+	
+}
