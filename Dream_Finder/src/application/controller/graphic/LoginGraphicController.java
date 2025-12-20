f00d4d144eb6cb32ee1e62a63465380b6a836d92
LorenzoGrasselli30 <lorenzo.grasselli30@gmail.com>
2025-12-20 18:37:06 +0100

    showPasswordButton

--- Dream_Finder/src/application/controller/graphic/LoginGraphicController.java
+++ Dream_Finder/src/application/controller/graphic/LoginGraphicController.java
@@ -1,21 +1,24 @@
 package application.controller.graphic;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import application.controller.application.LoginApplicationController;
 import application.exception.ValidationException;
 import application.view.AlertUtils;
 import application.view.WindowsNavigatorUtils;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 
-public class LoginGraphicController {
+public class LoginGraphicController implements Initializable {
 	
 	@FXML
 	private TextField emailField;
@@ -26,18 +29,66 @@ public class LoginGraphicController {
 	@FXML
 	private ImageView showPasswordButton;
 	
+	// Campo creato via codice, non presente nell'FXML
+	private TextField passwordTextField;
+	private boolean isPasswordVisible = false;
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		this.passwordTextField = new TextField();
+		
+		//Rende il textfield invisibile
+		this.passwordTextField.setVisible(false);
+		this.passwordTextField.setManaged(false);
+				
+		// Bind bidirezionale: se scrivi in uno, si aggiorna anche l'altro
+		this.passwordTextField.textProperty().bindBidirectional(passwordField.textProperty());
+		
+		this.passwordTextField.promptTextProperty().bind(passwordField.promptTextProperty());
+		this.passwordTextField.prefHeightProperty().bind(passwordField.prefHeightProperty());
+		this.passwordTextField.prefWidthProperty().bind(passwordField.prefWidthProperty());
+	}
+	
 	@FXML
     private void useLogin(MouseEvent event) throws IOException{
    	 
    	    switch (((Node) event.getSource()).getId()) {
    	        case "showPasswordButton":
+   	        	togglePasswordVisibility();
    	            break;
    	        case "signUpButton":
    	        	WindowsNavigatorUtils.openWindow(event, "signUpView.fxml", "Sign Up");
    	        	break;
    	    }
     }
-	 
+	
+	private void togglePasswordVisibility() {
+		// Recuperiamo il genitore (il layout che contiene la password)
+		Pane parent = (Pane) passwordField.getParent();
+		
+		// Se è la prima volta che clicchiamo, iniettiamo il TextField nel layout
+		if (!parent.getChildren().contains(passwordTextField)) {
+			int index = parent.getChildren().indexOf(passwordField);
+			parent.getChildren().add(index, passwordTextField);
+		}
+		
+		isPasswordVisible = !isPasswordVisible;
+
+		if (isPasswordVisible) {
+			// Mostra testo in chiaro
+			passwordTextField.setVisible(true);
+			passwordTextField.setManaged(true);
+			passwordField.setVisible(false);
+			passwordField.setManaged(false);
+		} else {
+			// Mostra password mascherata
+			passwordTextField.setVisible(false);
+			passwordTextField.setManaged(false);
+			passwordField.setVisible(true);
+			passwordField.setManaged(true);
+		}
+	}
+	
 	@FXML
 	private void doLogin(MouseEvent event) {
 		// Acquisisci i dati dalla View
@@ -65,13 +116,14 @@ public class LoginGraphicController {
 		}
 		
 		catch (ValidationException ve) {
-	           // Gestione specifica per errori di validazione
-		    	AlertUtils.showAlert(Alert.AlertType.WARNING, "Errore durante il login:", ve.getMessage());
+			// Gestione specifica per errori di validazione
+			AlertUtils.showAlert(Alert.AlertType.WARNING, "Errore durante il login:", ve.getMessage());
 	    }
 	    catch (Exception e) {
 	    	AlertUtils.showAlert(Alert.AlertType.ERROR, "Fatal error", "Qualcosa è andato storto, riprova più tardi.");		
 	    }
 
 	}
+
 }
 
