56ba60dc343f44856b412ce81d6b8bcf90ae822f
LorenzoGrasselli30 <lorenzo.grasselli30@gmail.com>
2025-12-26 18:13:32 +0100

    initPayment alpha

--- Dream_Finder/src/application/controller/graphic/PaymentGraphicController.java
+++ Dream_Finder/src/application/controller/graphic/PaymentGraphicController.java
@@ -11,6 +11,7 @@ import application.view.WindowsNavigatorUtils;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
@@ -33,6 +34,27 @@ public class PaymentGraphicController {
 	@FXML
 	private DatePicker dateField;
 	
+	@FXML
+	private Label providerNameLabel;
+	
+	@FXML
+	private Label nPeopleLabel;
+	
+	@FXML
+	private Label fullTicketLabel;
+	
+	@FXML
+	private Label reducedTicketLabel;
+	
+	@FXML
+	private Label shuttleServiceLabel;
+	
+	@FXML
+	private Label tourGuideLabel;
+	
+	@FXML
+	private Label totalLabel;
+	
 	public PaymentGraphicController() {
 		bookingController= new BookingApplicationController();
 	}	
@@ -40,6 +62,39 @@ public class PaymentGraphicController {
 	public void initPayment(BookingContext context) {
 		this.context= context;
 		
+		ActivityDTO activity = context.getActivity();
+		
+		// Inizializzazione label riepilogo
+		if (activity != null) { 
+			providerNameLabel.setText(activity.getProviderName());
+			
+			fullTicketLabel.setText("x" + context.getnFullTickets() + " Biglietto intero " 
+					+ String.format("%.2f", activity.getPrice()*context.getnFullTickets()) + "€"); 
+			
+			reducedTicketLabel.setText("x" + context.getnReducedTickets() + " Biglietto ridotto " 
+					+ String.format("%.2f", (activity.getPrice()/3.0)*context.getnReducedTickets()) + "€"); 
+		}
+		
+		int totalPeople = context.getnFullTickets() + context.getnReducedTickets();
+		nPeopleLabel.setText(String.valueOf(totalPeople));
+		
+		if (context.isShuttleService() && context.getShuttlePrice() != null) {
+			shuttleServiceLabel.setText("Servizio navetta: " + String.format("%.2f", context.getShuttlePrice()) + "€");
+		} else {
+			shuttleServiceLabel.setText("Servizio navetta: No");
+		}
+		
+		if (context.isGuideService() && context.getGuidePrice() != null) {
+			tourGuideLabel.setText("Tour guidato: " + String.format("%.2f", context.getGuidePrice()) + "€");
+		} else {
+			tourGuideLabel.setText("Tour guidato: No");
+		}
+		
+		if (context.getTotalPrice() != null) {
+			totalLabel.setText(String.format("%.2f", context.getTotalPrice()) + "€");
+		} else {
+			totalLabel.setText("0.00$");
+		}
 		
 	}
 	
