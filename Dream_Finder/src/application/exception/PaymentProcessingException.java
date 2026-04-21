99a5c456d488d08e1f03c3501e9bee531108bb8d
LorenzoGrasselli30 <lorenzo.grasselli30@gmail.com>
2026-04-21 16:09:08 +0200

    PaymentProcessingException

--- Dream_Finder/src/application/exception/PaymentProcessingException.java
+++ Dream_Finder/src/application/exception/PaymentProcessingException.java
@@ -1,7 +1,5 @@
 package application.exception;
 
-import com.stripe.exception.StripeException;
-
 public class PaymentProcessingException extends Exception{
 	
 	private static final long serialVersionUID = 1L;
