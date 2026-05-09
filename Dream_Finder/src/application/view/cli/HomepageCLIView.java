369aa3b5dfcc9a2f59683f6a371329ef6ca11dea
LorenzoGrasselli30 <lorenzo.grasselli30@gmail.com>
2026-05-09 12:38:16 +0200

    HomepageCLIView

--- Dream_Finder/src/application/view/cli/HomepageCLIView.java
+++ Dream_Finder/src/application/view/cli/HomepageCLIView.java
@@ -14,20 +14,62 @@ public class HomepageCLIView implements StartCLI {
 
 	@Override
 	public void start() {
-		System.out.println("|#### Homepage ####|");
-		System.out.println("1) Prenota un'attività");
-		System.out.println("2) Scopri l'attività che fa per te");
-		System.out.println("3) Cerca un itinerario");
-		System.out.println("4) Effettua il login");
-		System.out.println("5) Crea un itinerario");
-		System.out.println("6) Crea un attività");
-		System.out.println("7) Esci dal programma");
-		System.out.println("Inserisci la tua scelta (es. '4' per effettuare il login):");
+		Boolean running = true;
 		
-		String choice = scanner.nextLine();
-		
+		while (running) {
+			System.out.println("\n");
+			System.out.println("|#### Homepage ####|");
+			System.out.println("1) Prenota un'attività");
+			System.out.println("2) Scopri l'attività che fa per te");
+			System.out.println("3) Cerca un itinerario");
+			System.out.println("4) Effettua il login");
+			System.out.println("5) Crea un itinerario");
+			System.out.println("6) Crea un attività");
+			System.out.println("7) Esci dal programma");
+			System.out.println("Inserisci la tua scelta (es. '4' per effettuare il login):");
+			
+			String choice = scanner.nextLine();
+			
+			switch (choice) {
+			case "1":
+				navigator.navigateToActivity();
+				break;
+			case "2":
+				navigator.navigateToRecommendedActivities();
+				break;
+			case "3":
+				System.out.println("Funzionalità in fase di sviluppo...");
+				pause();
+				break;
+			case "4":
+				navigator.navigateToLogin();
+				break;
+			case "5":
+				System.out.println("Funzionalità in fase di sviluppo...");
+				pause();
+				break;
+			case "6":
+				System.out.println("Funzionalità in fase di sviluppo...");
+				pause();
+				break;
+			case "7":
+				System.out.println("Uscita dal programma in corso. A presto!");
+				running = false; 
+				System.exit(0);
+				break;
+			default:
+				System.out.println("Scelta non valida. Riprova.");
+				pause();
+			}
+		}
 		
 		
 	}
 	
+	public void pause() {
+	    System.out.println("Premi Invio per continuare...");
+	    if (scanner.hasNextLine()) {
+	        scanner.nextLine();
+	    }
+	}
 }
